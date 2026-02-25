05a63d30bdc8b28fb21d7b8cd8270a7000eae641
BengoPop <retr0astr4@gmail.com>
2026-02-24 22:57:44 -0500

    created getComputerChoice function deleted unused link to style.css

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-//
 //GOAL CREATE ROCK PAPER SCISSORS
-//GOAL WRITE LOGIC TO GET COMpUTER choice
 function getComputerChoice() {
-  //do not use arrays 
-  //math.random returns random number reater than oe equal to zero and less than 1 so basically 0 <= x < 1.
+  let randomChoiceno = Math.floor(Math.random() * 3);
+  let computerChoiceNo = randomChoiceno;
 
-  //returns string rock paper or scissors
+  switch (computerChoiceNo) {
+    case 0:
+      return "rock";
+
+    case 1:
+
+      return "paper";
+    case 2:
+
+      return "scissors";
+  }
 }
 
-while (true) {
-  console.log(Math.random())
-}
+
