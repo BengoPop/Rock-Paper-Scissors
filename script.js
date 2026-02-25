f75e283c24c3309c64476d5ddc83402d2d5df570
BengoPop <retr0astr4@gmail.com>
2026-02-24 22:50:06 -0500

    Initial commit plus some tips

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("hello");
+//
+//GOAL CREATE ROCK PAPER SCISSORS
+//GOAL WRITE LOGIC TO GET COMpUTER choice
+function getComputerChoice() {
+  //do not use arrays 
+  //math.random returns random number reater than oe equal to zero and less than 1 so basically 0 <= x < 1.
+
+  //returns string rock paper or scissors
+}
+
+while (true) {
+  console.log(Math.random())
+}
